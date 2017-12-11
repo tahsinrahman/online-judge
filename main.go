eab89d997c318e8c49b38e7b7c9dee60565e1601
tahsin <tahsynx@bitbucket.com>
2017-12-11 21:19:22 +0600

    fixed navbar, added signin, signup

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "gopkg.in/macaron.v1"
+import (
+	"gopkg.in/macaron.v1"
+)
 
 func main() {
 	m := macaron.Classic()
@@ -8,8 +10,28 @@ func main() {
 
 	m.Get("/", func(ctx *macaron.Context) {
 		ctx.Data["Login"] = 0
-		ctx.HTML(200, "index") // 200 is the response code.
+		ctx.HTML(200, "index")
 	})
 
-	m.Run()
+	m.Get("/signup", func(ctx *macaron.Context) {
+		ctx.HTML(200, "signup")
+	})
+	m.Post("/signup", func(ctx *macaron.Context) {
+		//insert to DB
+		ctx.Redirect(ctx.Req.URL.Host, 301)
+	})
+
+	m.Get("/signin", func(ctx *macaron.Context) {
+		ctx.HTML(200, "signin")
+	})
+	m.Post("/signin", func(ctx *macaron.Context) {
+		//insert to DB
+		ctx.Redirect(ctx.Req.URL.Host, 301)
+	})
+
+	m.Get("/signout", func(ctx *macaron.Context) {
+		ctx.HTML(200, "signout")
+	})
+
+	m.Run(8000)
 }
